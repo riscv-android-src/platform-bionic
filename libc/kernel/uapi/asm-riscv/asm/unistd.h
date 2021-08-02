eb977ba94145eb0ddf27ca6e42039b641cf3560c
CHEN GUOYIN <chenguoyin.cgy@alibaba-inc.com>
2021-08-02 14:22:09 +0800

    fix the issue as getrlimit not defined for riscv
    Traceback (most recent call last):
      File "/home/chenguoyin.cgy/android-s-beta/out/soong/.temp/Soong.python_2msafrtn/genfunctosyscallnrs.py", line 73, in <module>
        main()
      File "/home/chenguoyin.cgy/android-s-beta/out/soong/.temp/Soong.python_2msafrtn/genfunctosyscallnrs.py", line 67, in main
        gen_syscall_nrs(out_file=output_file,
      File "/home/chenguoyin.cgy/android-s-beta/out/soong/.temp/Soong.python_2msafrtn/genfunctosyscallnrs.py", line 30, in gen_syscall_nrs
        str(syscall_NRs[arch][syscall]) + ";\n")
    KeyError: 'getrlimit'
    
    Signed-off-by: CHEN GUOYIN <chenguoyin.cgy@alibaba-inc.com>
    Change-Id: Ic3ea92b3958988d36345645da9dba075088db796

--- libc/kernel/uapi/asm-riscv/asm/unistd.h
+++ libc/kernel/uapi/asm-riscv/asm/unistd.h
@@ -16,12 +16,13 @@
  ***
  ****************************************************************************
  ****************************************************************************/
-#ifdef __LP64__
+//syscall generation wont have the __LP64__ defined
+//#ifdef __LP64__
 #define __ARCH_WANT_RENAMEAT
 #define __ARCH_WANT_NEW_STAT
 #define __ARCH_WANT_SET_GET_RLIMIT
 #define __ARCH_WANT_SYS_CLONE3
-#endif
+//#endif
 #include <asm-generic/unistd.h>
 #ifndef __NR_riscv_flush_icache
 #define __NR_riscv_flush_icache (__NR_arch_specific_syscall + 15)
