533195785e6efac8b80187381d7109ecac5f785f
Mao Han <han_mao@linux.alibaba.com>
2021-11-23 11:00:01 +0800

    Add ucontext reg number definition
    Fix missing macro definition in libtextclassifier.
    
    Change-Id: Ie829a95746ce7b4ae5b8c11636f294422821eab9

--- libc/include/sys/ucontext.h
+++ libc/include/sys/ucontext.h
@@ -315,6 +315,15 @@ typedef struct ucontext {
 
 #elif __riscv_xlen == 64
 
+#define REG_PC 0
+#define REG_RA 1
+#define REG_SP 2
+#define REG_TP 4
+#define REG_S0 8
+#define REG_S1 9
+#define REG_A0 10
+#define REG_S2 18
+
 #define NGREG 32
 
 typedef unsigned long long greg_t;
