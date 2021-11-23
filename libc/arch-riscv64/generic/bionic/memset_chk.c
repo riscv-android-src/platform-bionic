4f5cdf1dc1853014f293628d7c9ad2a7bd3fe77c
Mao Han <han_mao@linux.alibaba.com>
2021-11-23 10:18:05 +0800

    Fix ifunc/static dispatch for mem/str optimization
    Change-Id: I56a6f7dbd1b6ad5b5b3822b05a533248f331bd2d

--- libc/arch-riscv64/generic/bionic/memset_chk.c
+++ libc/arch-riscv64/generic/bionic/memset_chk.c
@@ -4,7 +4,7 @@ void *memset(void * s, int c, unsigned long n);
 
 extern void* __memset_chk_fail();
 void *
-__memset_chk (void *dstpp, int c, unsigned long len, unsigned long dstlen)
+__memset_chk_generic (void *dstpp, int c, unsigned long len, unsigned long dstlen)
 {
   if (dstlen < len)
     __memset_chk_fail ();
--- libc/arch-riscv64/generic/bionic/strcmp.c
+++
@@ -1,8 +0,0 @@
-int
-strcmp (const char *s1, const char *s2)
-{
-  int ret;
-
-  while ((ret = *(unsigned char *) s1 - *(unsigned char*) s2++) == 0 && *s1++);
-  return ret;
-}
